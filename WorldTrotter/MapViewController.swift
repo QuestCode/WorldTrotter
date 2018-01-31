80eb054f3892a4cee3c516ac1909862c9907c286
QuestCode <omgitszslick@gmail.com>
2018-01-30 19:22:09 -0800

    added mapviewcontroller

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Devontae Reid on 1/30/18.
+//  Copyright © 2018 Devontae Reid. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
